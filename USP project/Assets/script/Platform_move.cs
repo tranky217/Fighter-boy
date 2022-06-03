0eaacbbc15cb8aedcc5e4b489fa7237847bb3611
tranky217 <trantrongky217@gmail.com>
2022-06-03 10:42:10 +0200

    first push

---
+++ USP project/Assets/script/Platform_move.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform_move : MonoBehaviour
+{
+    [SerializeField] private GameObject[] points;
+    private int currentPointIdx = 0;
+    // Start is called before the first frame update
+    [SerializeField] private float speed = 1.5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // check distance of current point and platform => moving
+        if (Vector2.Distance(points[currentPointIdx].transform.position, transform.position) < 1f)
+        {
+            currentPointIdx++;
+            if (currentPointIdx >= points.Length)
+            {
+                currentPointIdx = 0;
+            }
+        }
+        transform.position = Vector2.MoveTowards(transform.position, points[currentPointIdx].transform.position, Time.deltaTime * speed);
+    }
+}
